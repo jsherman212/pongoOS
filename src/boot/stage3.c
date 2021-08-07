5b9812036f9433e7a15f8388aa7bb5f6b158c56b
jsherman212 <justinsherman419@yahoo.com>
2021-08-07 16:54:44 -0400

    start rom pongo

--- src/boot/stage3.c
+++ src/boot/stage3.c
@@ -64,46 +64,50 @@ extern uint8_t clear_hook, clear_hook_end;
 
 void patch_bootloader(void* boot_image)
 {
-    // 1. disable DRAM clear
+    /* XXX XXX make this do nothing */
+    return;
+    /* // 1. disable DRAM clear */
 
-    uint32_t* sys_3_c7_c4_1 = find_next_insn(boot_image, 0x80000, 0xd50b7423, 0xFFFFFFFF);
-    if (sys_3_c7_c4_1) {
-        uint32_t* func_prolog = find_prev_insn(sys_3_c7_c4_1, 0x100, 0xaa0103e2, 0xffffffff);
-        if (func_prolog) {
-            for (int i = 0; i < 2; i++) {
-                clear_hook_orig_backing[i] = func_prolog[i];
-            }
-            memcpy(((void*)0x180 + (uint64_t)boot_image), &clear_hook, &clear_hook_end - &clear_hook);
-            int64_t offset = (0x180 + (int64_t)boot_image) - (4 + (int64_t)func_prolog);
-            func_prolog[0] = 0xaa1e03e5;
-            func_prolog[1] = 0x94000000 | ((((uint64_t)offset) >> 2) & 0x3FFFFFF);
-        }
-    }
-    invalidate_icache();
+    /* uint32_t* sys_3_c7_c4_1 = find_next_insn(boot_image, 0x80000, 0xd50b7423, 0xFFFFFFFF); */
+    /* if (sys_3_c7_c4_1) { */
+    /*     uint32_t* func_prolog = find_prev_insn(sys_3_c7_c4_1, 0x100, 0xaa0103e2, 0xffffffff); */
+    /*     if (func_prolog) { */
+    /*         for (int i = 0; i < 2; i++) { */
+    /*             clear_hook_orig_backing[i] = func_prolog[i]; */
+    /*         } */
+    /*         memcpy(((void*)0x180 + (uint64_t)boot_image), &clear_hook, &clear_hook_end - &clear_hook); */
+    /*         int64_t offset = (0x180 + (int64_t)boot_image) - (4 + (int64_t)func_prolog); */
+    /*         func_prolog[0] = 0xaa1e03e5; */
+    /*         func_prolog[1] = 0x94000000 | ((((uint64_t)offset) >> 2) & 0x3FFFFFF); */
+    /*     } */
+    /* } */
+    /* invalidate_icache(); */
 
-    // 2. hook trampoline to jump into this again
+    /* // 2. hook trampoline to jump into this again */
 
-    uint32_t* tramp = find_next_insn(boot_image, 0x80000, 0xd2800012, 0xFFFFFFFF);
-    if (tramp) {
-        for (int i = 0; i < 5; i++) {
-            tramp[i] = tramp_hook[i];
-        }
-    }
-//    d$demote_patch(boot_image);
+    /* uint32_t* tramp = find_next_insn(boot_image, 0x80000, 0xd2800012, 0xFFFFFFFF); */
+    /* if (tramp) { */
+    /*     for (int i = 0; i < 5; i++) { */
+    /*         tramp[i] = tramp_hook[i]; */
+    /*     } */
+    /* } */
+/* //    d$demote_patch(boot_image); */
 
-    iorvbar_yeet(boot_image);
-    aes_keygen(boot_image);
-    // Ultra dirty hack: 16K support = Reconfig Engine
-    if(is_16k())
-    {
-        recfg_yoink(boot_image);
-    }
+    /* iorvbar_yeet(boot_image); */
+    /* aes_keygen(boot_image); */
+    /* // Ultra dirty hack: 16K support = Reconfig Engine */
+    /* if(is_16k()) */
+    /* { */
+    /*     recfg_yoink(boot_image); */
+    /* } */
 
-    invalidate_icache();
+    /* invalidate_icache(); */
 }
 
 /* BSS is cleaned on _start, so we cannot rely on it. */
 void* gboot_entry_point = (void*)0xddeeaaddbbeeeeff;
+
+/* No boot args for ROM --> iBoot, XXX make them myself? */
 void* gboot_args = (void*)0xddeeaaddbbeeeeff;
 
 void stage3_exit_to_el1_image(void* boot_args, void* boot_entry_point) {
@@ -124,28 +128,21 @@ void trampoline_entry(void* boot_image, void* boot_args)
 {
     extern uint64_t __bss_start[] __asm__("section$start$__DATA$__common"),
                     __bss_end[] __asm__("segment$end$__DATA");
-    if (__bss_start[0] == 0x746F6F626F747561) {
-        uint32_t autoboot_sz = (uint32_t)(__bss_start[1]);
-        extern volatile void smemcpy128(void*,void*,uint32_t);
-        smemcpy128 ((void*)0x818e00000, __bss_start, (autoboot_sz + 64)/16);
-        __bss_start[0] = 0;
-    }
 
-    if (!boot_args) {
-        // bootloader
-        strcpy(boot_image + 0x200, "Stage2 KJC Loader");
-        patch_bootloader(boot_image);
-    } else {
+    gboot_args = boot_args;
+    gboot_entry_point = boot_image;
 
-        gboot_args = boot_args;
-        gboot_entry_point = boot_image;
-        extern volatile void setup_el1(void * entryp,uint64_t,uint64_t);
+    extern volatile void setup_el1(void * entryp,uint64_t,uint64_t);
+    extern volatile void smemset(void*, uint8_t, uint64_t);
+    smemset(&__bss_start, 0, ((uint64_t)__bss_end) - ((uint64_t)__bss_start));
+    extern void main (void);
 
+    /* This doesn't seem to return? */
+    /* XXX why does this take three parameters? TODO make sure removing
+     * the last two params doesn't cause any crashes after getting
+     * this working */
+    setup_el1(main, (uint64_t)boot_image, (uint64_t)boot_args);
 
-        extern volatile void smemset(void*, uint8_t, uint64_t);
-        smemset(&__bss_start, 0, ((uint64_t)__bss_end) - ((uint64_t)__bss_start));
-        extern void main (void);
-        setup_el1(main, (uint64_t)boot_image, (uint64_t)boot_args);
-    }
-    jump_to_image((uint64_t)boot_image, (uint64_t)boot_args);
+    /* Not reached? */
+    /* jump_to_image((uint64_t)boot_image, (uint64_t)boot_args); */
 }
